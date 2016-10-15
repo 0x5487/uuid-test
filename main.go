fe16615add86218fd476bda9aea2d35abbf7cdd0
Jason Lee <jawc@hotmail.com>
2016-10-15 16:30:10 +0800

    use goroutines

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
-import uuid "github.com/satori/go.uuid"
+import (
+	"sync"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var (
+	mutex = &sync.Mutex{}
+	wg    = &sync.WaitGroup{}
+)
 
 func main() {
 	store := map[string]string{}
+	total := 1000000
+	wg.Add(total)
 
-	for i := 0; i < 5000000; i++ {
-		uuid := uuid.NewV4().String()
-
-		if _, ok := store[uuid]; ok {
-			println("duplicate:", uuid)
-		} else {
-			store[uuid] = ""
-		}
+	startTime := time.Now()
+	for i := 0; i < total; i++ {
+		go createUUID(store)
 	}
-
+	wg.Wait()
+	duration := int64(time.Since(startTime) / time.Millisecond)
 	println("total count:", len(store))
+	println("duration:", duration)
+}
+
+func createUUID(store map[string]string) {
+	uuid := uuid.NewV4().String()
+	mutex.Lock()
+	if _, ok := store[uuid]; ok {
+		println("duplicate:", uuid)
+	} else {
+		store[uuid] = ""
+	}
+	mutex.Unlock()
+	wg.Done()
 }
